30cafcecb53f20082c12ec65d58e312eb25df714
Olgu Değirmenci <92662245+OlguD@users.noreply.github.com>
2024-06-16 23:19:26 +0300

    Update test_login_v2.py

--- test_login_v2.py
+++ test_login_v2.py
@@ -1,7 +1,7 @@
 from get_cookie import is_two_factor_required, login_v2
 
 
-two_factor_bool, response_json = is_two_factor_required('somethingmore1219@gmail.com',
-                                                        'somethingmore121920')
+two_factor_bool, response_json = is_two_factor_required('x',
+                                                        'x')
 
 print(two_factor_bool, response_json)
