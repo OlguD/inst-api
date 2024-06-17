3d216a92bf899200b282f097a1b7d4d3655aec6f
Olgu Değirmenci <92662245+OlguD@users.noreply.github.com>
2024-06-17 03:22:35 +0300

    Update test_flask_app.py

--- test/test_flask_app.py
+++ test/test_flask_app.py
@@ -2,8 +2,8 @@ import requests
 
 host_session = "http://127.0.0.1:8090/is_session_valid"
 host_login = "http://127.0.0.1:8090/login"
-data = {'username': 'bionlukbionluk400@gmail.com',
-        'password': 'Bionluk_bionluk40020'}
+data = {'username': 'x@gmail.com',
+        'password': 'x'}
 
 request = requests.post(host_login, json=data)
 print(request.text, request.status_code)
